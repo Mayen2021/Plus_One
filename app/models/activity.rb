951ecd489cf2fa9b8a0e26b5bf8b5a6c5e05838d
Johann Alerte <johann.alerte@gmail.com>
2021-06-09 17:04:55 +0100

    mapping

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,6 +3,10 @@ class Activity < ApplicationRecord
   has_many :bookings, dependent: :destroy
   belongs_to :theme
   has_one_attached :photo
+  geocoded_by :location
+
 
   validates :title, presence: true
+  after_validation :geocode, if: :will_save_change_to_location?
+
 end
