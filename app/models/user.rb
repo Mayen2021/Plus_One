4eb98957aa7a13185f13a5e106052afbd3d05986
Mayen Enodien <menodien@gmail.com>
2021-06-15 13:00:16 +0100

    changes made

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :profile, dependent: :destroy
+  has_many :activities
+  has_many :bookings
 end
